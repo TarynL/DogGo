7ddc62a227df853b205c89efec0a9724329332b6
TarynL <tarlytle@gmail.com>
2021-06-23 16:36:19 -0500

    working on total duration for walkers details

--- Controllers/WalkersController.cs
+++ Controllers/WalkersController.cs
@@ -1,4 +1,5 @@
 ﻿using DogGo.Models;
+using DogGo.Models.ViewModels;
 using DogGo.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -12,11 +13,13 @@ namespace DogGo.Controllers
     public class WalkersController : Controller
     {
         private readonly IWalkerRepository _walkerRepo;
+        private readonly IWalksRepository _walksRepo;
 
         // ASP.NET will give us an instance of our Walker Repository. This is called "Dependency Injection"
-        public WalkersController(IWalkerRepository walkerRepository)
+        public WalkersController(IWalkerRepository walkerRepository, IWalksRepository walksRepository)
         {
             _walkerRepo = walkerRepository;
+            _walksRepo = walksRepository;
         }
 
         // GET: WalkersController
@@ -30,22 +33,37 @@ namespace DogGo.Controllers
 
 
         // GET: Walkers/Details/5
+        //public ActionResult Details(int id)
+        //{
+        //    Walker walker = _walkerRepo.GetWalkerById(id);
+
+        //    if (walker == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return View(walker);
+        //}
+
         public ActionResult Details(int id)
         {
             Walker walker = _walkerRepo.GetWalkerById(id);
+            List<Walks> walks = _walksRepo.GetAllWalksByWalkerId(walker.Id);
 
-            if (walker == null)
+            WalkerProfileViewModel wpvm = new WalkerProfileViewModel()
             {
-                return NotFound();
-            }
+                Walker = walker,
+                Walks = walks
+            };
+            return View(wpvm);
 
-            return View(walker);
         }
 
 
         // GET: WalkersController/Create
         public ActionResult Create()
         {
+
             return View();
         }
 
